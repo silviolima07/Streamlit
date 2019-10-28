1d5e32f961ce7dea8f999a0c2e64814a89a545c6
silviolima07 <silviolima07@gmail.com>
2019-10-28 15:38:30 -0400

    Iris EDA app deploy

--- iris_eda_app.py
+++ iris_eda_app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
 def main():
-    """ Iris EDA App """
-    
+    """ A simple iris EDA App """
+
     st.title("Iris EDA App with streamlit")
-    st.subheader("Streamlit is cool")
-    
-if __name__== '__main__':
+    st.subheader("Streamlit is python server for web apps")
+
+if __name__ == '__main__':
     main()
