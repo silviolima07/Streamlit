476c5dc2c49782ded99949c6ad3d76be25fc9b57
silviolima07 <silviolima07@gmail.com>
2019-10-29 18:36:54 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,7 @@
 mkdir -p ~/.streamlit/
 
-echo "\
-[general]\n\
-email = \"silcesarlima@yahoo.com.br\"\n\
-" > ~/.streamlit/credentials.toml
 
+echo "config.toml"
 echo "\
 [server]\n\
 headless = true\n\
@@ -12,6 +9,12 @@ enableCORS=false\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
 
+echo "credencial.toml"
+echo "\
+[general]\n\
+email = \"silcesarlima@yahoo.com.br\"\n\
+" > ~/.streamlit/credentials.toml
+
 
 echo "PORTA USADA:" $PORT
-echo "enableCORS:"enableCORS
+echo "enableCORS:"$enableCORS
