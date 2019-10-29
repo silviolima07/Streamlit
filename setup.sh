51c148babce92342a53857e5dd4e03f37a71c97c
silviolima07 <silviolima07@gmail.com>
2019-10-29 13:04:47 -0300

    Iris EDA deploy

--- setup.sh
+++ setup.sh
@@ -1,9 +1,12 @@
 mkdir -p ~/.streamlit/
+echo "\
+[general]\n\
+email = \"silcesarlima@yahoo.com.br\"\n\
+" > ~/.streamlit/credentials.toml
 
 echo "\
 [server]\n\
 headless = true\n\
+enableCORS=false\n\
 port = $PORT\n\
-enableCORS = false\n\
-\n\
 " > ~/.streamlit/config.toml
