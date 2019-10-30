df0d4d34c1a361716ced6af12c096de3e827c7ad
silviolima07 <silviolima07@gmail.com>
2019-10-30 10:49:12 -0300

    Update setup.sh

--- iris_eda_app.py
+++ iris_eda_app.py
@@ -1,12 +1,16 @@
 import streamlit as st
 import os
+#import app
 
 def main():
     """ A simple iris EDA App """
 
     st.title("Iris EDA App with streamlit")
     st.subheader("Streamlit is a python server for web apps")
+    #port = int(os.environ.get('PORT', 5000))
+
     port = int(os.environ.get('PORT', 5000))
+    print(port)
 
 if __name__ == '__main__':
     main()
