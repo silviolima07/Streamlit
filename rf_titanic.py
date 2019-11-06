be64d5a49a90a78a6a65dcf43c882f10463b2501
silviolima07 <silviolima07@gmail.com>
2019-11-06 18:44:24 -0300

    new version

--- rf_titanic.py
+++ rf_titanic.py
@@ -1,11 +1,13 @@
 import streamlit as st
 import pickle
 import pandas as pd
+import numpy as np
 import os
 from sklearn.preprocessing import OneHotEncoder,LabelEncoder
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
 import time
+import plotly.express as px
 
 
 
@@ -13,77 +15,69 @@ def main():
     """ RandonForestClassifier - Titanic """
     
     # Titulo
-    st.title("	STREAMLIT	")
+    st.title("	STREAMLIT APPS	")
+    #st.markdown("## Silvio Lima")
     st.title("RandomForestClassifier / Titanic")
-    st.markdown("## Silvio Lima")
-
-    st.sidebar.title("About")
-
-    st.sidebar.info(
-    "Streamlit é uma biblioteca do Python que torna fácil construir apps web. Semelhante ao Shiny usado com R. Veja mais em: https://github.com/streamlit/streamlit e https://streamlit.io/")
-
-    # Carregar dataset
-    my_dataset = "dataset.csv"
-    st.text(" Dataset Titanic - 891 observações / 6 atributos")
-    st.text("Atributos: Status, Classe, Sexo, Idade, Passagem, Embarque")
     
+
+    st.sidebar.title('EDA & Classificação')
+
+    st.sidebar.text('Titanic - 891 obs. / 6 atributos')
+
+    st.sidebar.markdown('Selecione uma opção:')
+
     def ler_dados(dataset):
         df = pd.read_csv(os.path.join(dataset))
         return df
-
-    if st.checkbox("Preview Dataset"):
-        data = ler_dados(my_dataset)
-        if st.button("Head"):
-            st.write(data.head())
     
-    st.text("Modelo gerado para classificar se o passageiro:")
-    st.write("SOBREVIVEU ou MORREU")
-    st.text("O modelo obteve uma acurácia de 83.58%")
+    # Carregar dataset
+    my_dataset = "dataset.csv"
+    data = ler_dados(my_dataset)
     
-    filename = "RFmodel.sav"
-    
-    rfmodel = pickle.load(open(filename,'rb'))
-
-    st.markdown("## Selecione as caracteristicas do passageiro")
-
-    classe = st.radio('Classe',('Primeira Classe ', 'Segunda Classe', 'Terceira Classe'))
-
-    sexo = st.radio('Sexo',('Homem', 'Mulher'))
-
-    embarque = st.radio('Cidade de Embarque',('Cherboug', 'Queenstown', 'Southampton'))
-
-    idade = st.slider('Idade',min_value=1, max_value=80, value=20, step=5)
-  
-    passagem = st.slider('Valor da Passagem',min_value=0, max_value=512, value=100, step=50)
-
-    st.markdown("## Caracteristicas selecionadas do passageiro")
-
-    st.write(classe,'---', sexo, '---',embarque,'---',idade,"anos",'---','$$',passagem)
     
 
-    data = [{'Classe': classe, 'Sexo': sexo, 'Embarque':embarque, 'Idade': idade, 'Passagem': passagem}]
+    if st.sidebar.checkbox("Preview Dataset"):      
+        st.write("Preview")
+        st.write(data.head())
+    
+    if st.sidebar.checkbox("Distribuiçao dos dados"):      
+        x = st.sidebar.selectbox('Atributo: ', ['Status','Classe', 'Sexo', 'Idade', 'Embarque', 'Passagem'])      
+        st.write("Histograma:  ",x)
+        fig1 = px.histogram(data, x=x, hover_data=data.columns, color=x)
+        st.plotly_chart(fig1)
+        
+    if st.sidebar.checkbox('Previsão / Classificação'):
+        filename = "RFmodel.sav"
+        rfmodel = pickle.load(open(filename,'rb'))
+        st.sidebar.markdown("Selecione as caracteristicas do passageiro")
+        classe = st.sidebar.radio('Classe',('Primeira Classe ', 'Segunda Classe', 'Terceira Classe'))
+        sexo = st.sidebar.radio('Sexo',('Homem', 'Mulher'))
+        embarque = st.sidebar.radio('Cidade de Embarque',('Cherboug', 'Queenstown', 'Southampton'))
+        idade = st.sidebar.slider('Idade',min_value=1, max_value=80, value=20, step=5)
+        passagem = st.sidebar.slider('Valor da Passagem',min_value=0, max_value=512, value=100, step=10)
+        st.markdown("## Caracteristicas selecionadas do passageiro")
+        st.write(classe,'---', sexo, '---',embarque,'---',idade,"anos",'---','$$',passagem)
+        st.markdown("Confira as caracteristicas do passageiro no Preview")
+        data = [{'Classe': classe, 'Sexo': sexo, 'Embarque':embarque, 'Idade': idade, 'Passagem': passagem}]
+        df = pd.DataFrame(data)
+        status = rfmodel.predict(df)
+        bar = st.progress(0)
 
-    df = pd.DataFrame(data)
- 
-    if (classe == "Primeira Classe" and sexo == "Homem" and embarque == "Cherboug" and idade == 20 and passagem == 512):
-        status = "Nada"
-     
-    status = rfmodel.predict(df)
-
-    time.sleep(1)
-
-    if (status == 'Sobreviveu'):
-        st.markdown('# Sobreviveu')
-        #st.balloons()
-    else:
-        st.markdown('# Morreu')
-        st.write(" no acidente")
+        if st.button('Submit'):
+            for i in range(11):
+                bar.progress(i * 10)
+                # wait
+                time.sleep(0.1)
+            st.markdown('#### De acordo com o modelo:')
+            if (status == 'Sobreviveu'):
+                st.markdown('# Sobreviveu')
+                st.write("vamos comemorar!")
+                st.balloons()
+            else:
+                st.markdown('# Morreu')
+                st.write(" infelizmente no acidente")
         
     
-    #st.markdown("Parametros do modelo gerado")
-    #st.write(rfmodel)
-    
-
 
 if __name__ == '__main__':
     main()
